5c147cf078dbf6e8e774037f4fc9008e6ac61a30
Zr0den <axellund7@gmail.com>
2024-10-03 16:04:46 +0700

    API og MessageClient connection

--- SysAPI/Controllers/ValutaController.cs
+++ SysAPI/Controllers/ValutaController.cs
@@ -1,33 +1,82 @@
+using Helpers.Messages;
+using MessageClient;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SysAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ValutaController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private readonly MessageClient<ValutaResponseMessage> _valutaResponseMessageClient;
+        private readonly MessageClient<ValutaRequestMessage> _valutaRequestMessageClient;
 
-        private readonly ILogger<ValutaController> _logger;
-
-        public ValutaController(ILogger<ValutaController> logger)
+        public ValutaController(MessageClient<ValutaResponseMessage> valutaResponseMessageClient, MessageClient<ValutaRequestMessage> valutaRequestMessageClient)
         {
-            _logger = logger;
+            _valutaResponseMessageClient = valutaResponseMessageClient;
+            _valutaRequestMessageClient = valutaRequestMessageClient;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<ValutaItems> Get()
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //private readonly ILogger<ValutaController> _logger;
+
+        //public ValutaController(ILogger<ValutaController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<ValutaItems> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new ValutaItems
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        internal static class MessageWaiter
         {
-            return Enumerable.Range(1, 5).Select(index => new ValutaItems
+            public static async Task<T?>? WaitForMessage<T>(MessageClient<T> messageClient, string clientId, int timeout = 5000)
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var tcs = new TaskCompletionSource<T?>();
+                var cancellationTokenSource = new CancellationTokenSource(timeout);
+                cancellationTokenSource.Token.Register(() => tcs.TrySetResult(default));
+
+                using (
+                    var connection = messageClient.ListenUsingTopic<T>(message =>
+                    {
+                        tcs.TrySetResult(message);
+                    }, "customer" + clientId, clientId)
+                )
+                {
+                }
+
+                return await tcs.Task;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> PostValutaExchange(ValutaItems valuta)
+        {
+            _valutaRequestMessageClient.SendUsingTopic(new ValutaRequestMessage
+            {
+                ValutaId = valuta.ValutaId,
+                Status = valuta.Status,
+                CurrencyCode = valuta.CurrencyCode,
+                Value = valuta.Value,
+            }, "newValue");
+
+            var response = await MessageWaiter.WaitForMessage(_valutaResponseMessageClient, valuta.ValutaId.ToString())!;
+
+            return response != null ? response.Status : "Transaction timed out";
+
         }
 
 
